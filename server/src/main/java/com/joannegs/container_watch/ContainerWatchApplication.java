7070137afb10e2a7cef47f755e3ea753e9b931d9
Joanne Silva <joanneegabriela@gmail.com>
2024-09-27 01:50:15 -0300

    feat(server): first commit

---
+++ server/src/main/java/com/joannegs/container_watch/ContainerWatchApplication.java
@@ -0,0 +1,13 @@
+package com.joannegs.container_watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerWatchApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/joannegs/container_watch/ContainerWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joannegs.container_watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
